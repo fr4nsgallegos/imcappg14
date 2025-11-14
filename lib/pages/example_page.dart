4a497c48e814380bb1328d1c3230972456a21e57
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2025-11-13 22:21:44 -0500

    modifiicando texto a partir de sliders

--- lib/pages/example_page.dart
+++ lib/pages/example_page.dart
@@ -10,6 +10,28 @@ class ExamplePage extends StatefulWidget {
 class _ExamplePageState extends State<ExamplePage> {
   double textSize = 10;
   double red = 0, green = 0, blue = 0, opacity = 1.0;
+  bool isUnderline = false, isOverLine = false, isLineThrough = false;
+  Widget _buildSlider(
+    String title,
+    double value,
+    Color color,
+    Function(double) onchanged, {
+    bool isOpacity = false,
+  }) {
+    return Column(
+      children: [
+        Text(title),
+        Slider(
+          max: isOpacity ? 1 : 255,
+          min: 0,
+          value: value,
+          activeColor: color,
+          onChanged: onchanged,
+        ),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,6 +43,11 @@ class _ExamplePageState extends State<ExamplePage> {
             Text(
               "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley",
               style: TextStyle(
+                decoration: TextDecoration.combine([
+                  if (isUnderline) TextDecoration.underline,
+                  if (isOverLine) TextDecoration.overline,
+                  if (isLineThrough) TextDecoration.lineThrough,
+                ]),
                 fontSize: textSize,
                 color: Color.fromRGBO(
                   red.toInt(),
@@ -42,29 +69,77 @@ class _ExamplePageState extends State<ExamplePage> {
                 });
               },
             ),
-            Text("Color Rojo"),
-            Slider(
-              max: 255,
-              min: 0,
-              value: red,
-              activeColor: Colors.red,
-              onChanged: (v) {
-                setState(() {
-                  red = v;
-                });
-              },
+            _buildSlider(
+              "Color Rojo",
+              red,
+              Colors.red,
+              (v) => setState(() {
+                red = v;
+              }),
             ),
-            Text("Color verde"),
-            Slider(
-              max: 255,
-              min: 0,
-              value: green,
-              activeColor: Colors.green,
-              onChanged: (v) {
-                setState(() {
-                  green = v;
-                });
-              },
+            _buildSlider(
+              "Color Azul",
+              blue,
+              Colors.blue,
+              (v) => setState(() {
+                blue = v;
+              }),
+            ),
+            _buildSlider(
+              "Color verde",
+              green,
+              Colors.green,
+              (v) => setState(() {
+                green = v;
+              }),
+            ),
+            _buildSlider(
+              "Opacidad",
+              opacity,
+              isOpacity: true,
+              Colors.black,
+              (p0) => setState(() {
+                opacity = p0;
+              }),
+            ),
+            Divider(),
+            CheckboxListTile(
+              value: isUnderline,
+              title: Text("Subrayado"),
+              subtitle: Text("Subrayar texto"),
+              secondary: Icon(Icons.format_underline),
+              activeColor: Colors.red,
+              checkColor: Colors.white,
+              // controlAffinity: ListTileControlAffinity.leading, //Para que aparezcael chekbox a la izquierda
+              // contentPadding: EdgeInsets.all(16),
+              // dense: true ,  //Más compacto
+              selected: isUnderline, //Se marca visualmente todo el widget
+              // onChanged:null, //inhabilita el chekbox y listile
+              onChanged: (value) => setState(() {
+                isUnderline = value!;
+              }),
+            ),
+            CheckboxListTile(
+              value: isOverLine,
+              title: Text("Raya encima"),
+              subtitle: Text("Raya encima texto"),
+              secondary: Icon(Icons.format_overline),
+              activeColor: Colors.cyan,
+              selected: isOverLine, //Se marca visualmente todo el widget
+              onChanged: (value) => setState(() {
+                isOverLine = value!;
+              }),
+            ),
+            CheckboxListTile(
+              value: isLineThrough,
+              title: Text("Tachar"),
+              subtitle: Text("Tachar texto"),
+              secondary: Icon(Icons.line_weight_rounded),
+              activeColor: Colors.blue,
+              selected: isLineThrough, //Se marca visualmente todo el widget
+              onChanged: (value) => setState(() {
+                isLineThrough = value!;
+              }),
             ),
           ],
         ),
